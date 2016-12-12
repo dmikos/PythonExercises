550620a234b6518df621645ba1264e3c73143811
Dmitriy Kos <dkostinov@gmail.com>
2016-12-12 17:37:13 +0200

    flask - MegaBook

---
+++ microblog/README.txt
@@ -0,0 +1,35 @@
+https://habrahabr.ru/post/193242/
+flask/bin/pip install flask==0.9
+flask/bin/pip install flask-login
+flask/bin/pip install flask-openid
+flask/bin/pip install flask-mail
+flask/bin/pip install sqlalchemy==0.7.9
+flask/bin/pip install flask-sqlalchemy==0.16
+flask/bin/pip install sqlalchemy-migrate
+flask/bin/pip install flask-whooshalchemy==0.54a
+flask/bin/pip install flask-wtf==0.8.4
+flask/bin/pip install pytz==2013b
+flask/bin/pip install flask-babel==0.8
+flask/bin/pip install flup
+
+Это первая статья в серии, где я буду документировать мой опыт написания веб-приложения на Python, используя микрофреймворк Flask.
+
+Здесь список всех статей в серии:
+Часть 1: Привет, Мир!
+Часть 2: Шаблоны
+Часть 3: Формы
+Часть 4: База данных
+Часть 5: Вход пользователей
+Часть 6: Страница профиля и аватары
+Часть 7: Unit-тестирование
+Часть 8: Подписчики, контакты и друзья
+Часть 9: Пагинация
+Часть 10: Полнотекстовый поиск
+Часть 11: Поддержка e-mail
+Часть 12: Реконструкция
+Часть 13: Дата и время
+Часть 14: I18n and L10n
+Часть 15: Ajax
+Часть 16: Отладка, тестирование и профилирование
+Часть 17: Развертывание на Linux (даже на Raspberry Pi!)
+Часть 18: Развертывание на Heroku Cloud
--- parse/parser.py
+++ parse/parser.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from urllib.parse import urljoin
 from lxml.html import fromstring
+import xlsxwriter
 
 # url = https://geekbrains.ru/courses#cour
 # class="gb__main-wrapper"
@@ -15,7 +16,7 @@ URL = 'https://geekbrains.ru/courses#cour'
 ITEM_PATH = '.row .course-item .course-item__description'
 DESCR_PATH1 = '.gb__main-wrapper .div padder-v m-r m-l .row m-t-lg .col-md-6 .author .pull-left'
 DESCR_PATH = '.col-md-6 .author .pull-left'
-TEACH_PATH = '#authors .author .m-l m-b-xs m-t-xs m-r-xs'
+TEACH_PATH = '#authors .author .m-l'
 
 
 
@@ -23,6 +24,7 @@ def parse_courses():
     f = urlopen(URL)
     list_html = f.read().decode('utf-8')
     list_doc = fromstring(list_html)
+    courses = []
 
     for elem in list_doc.cssselect(ITEM_PATH):
         a = elem.cssselect('a.course-item__description__title')[0]
@@ -34,20 +36,38 @@ def parse_courses():
 
         course = {'name': name, 'lead': description, 'url': url}
 
-        details_html = urlopen(url).read().decode('utf-8')
-        details_doc = fromstring(details_html)
+        # details_html = urlopen(url).read().decode('utf-8')
+        # details_doc = fromstring(details_html)
 
-        for teach_elems in details_doc.cssselect(TEACH_PATH):
-            teaches = [teach_elems.text for teach_elem in teach_elems]
-            print(teaches)
-        # teach_elems = details_doc.cssselect(TEACH_PATH)
-        # teaches = [teach_elems.text for teach_elem in teach_elems]
+        # teaches = details_doc.cssselect(TEACH_PATH)
+        # for teach_elems in details_doc.cssselect(TEACH_PATH):
+        #    teaches = [teach_elems.text for teach_elem in teach_elems]
+        #    print(teaches)
+        courses.append(course)
+    return courses
 
+def export_excel(filename, courses):
+    workbook = xlsxwriter.Workbook(filename)
+    worksheet = workbook.add_worksheet()
+    # worksheet.write(1, 2, 'Hello Excel')  #строка №1, колонка №2, текст в ячейке - "Hello Excel"
 
+    bold = workbook.add_format({'bold': True})
 
+    field_names = ('Название', 'Описание', 'url')
+    for i, filed in enumerate(field_names):
+        worksheet.write(0, i, filed, bold)
+
+    fields = ('name', 'lead', 'url')
+    for row, course in enumerate(courses, start=1):
+        for col, field in enumerate(fields):
+            #print(row, col, course[field])
+            worksheet.write(row, col, course[field])
+
+    workbook.close()
 
 def main():
-    parse_courses()
+    courses = parse_courses()
+    export_excel('courses.xlsx', courses)
 
 if __name__ == '__main__':
     main()
