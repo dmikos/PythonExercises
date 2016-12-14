769776fceee7a52b2b58fc4cda8fc8625f54cf59
Dmitriy Kos <dkostinov@gmail.com>
2016-12-14 13:37:38 +0200

    flask - MegaBook - microblog

--- microblog.task2/app/views.py
+++ microblog.task2/app/views.py
@@ -1,6 +1,6 @@
 from . import app
-from flask import render_template, flash, redirect
-from .forms import LoginForm
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
@@ -20,10 +20,3 @@ def index():
         title = 'Home',
         user = user,
         posts = posts)
-
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    form = LoginForm()
-    return render_template('login.html',
-        title = 'Sign In',
-        form = form)
